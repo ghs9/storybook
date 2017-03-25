1e0a8620efb4d787fbe2ad2ffbb2e72d7f6e3c15
ghs9 <ghs9@uw.edu>
2017-03-25 11:38:36 -0700

    changed file name of output

--- storyBook.py
+++ storyBook.py
@@ -1,7 +1,3 @@
-# Gabriel Summers ghs9@uw.edu
-# 3/8/17
-# TCSS435
-# Assignment 3
 # storyBook.py
 
 import random
@@ -39,7 +35,7 @@ def prepTrigramsFromFile(file_name):
 # occuring following the second and first word.
 # The printing of the second and third word is
 # repeated 500 times, resulting in a story that
-# is 1000 lines long.
+# is aprox. 1000 lines long.
 #
 # If the output file is output_complex.txt
 # then all files are expected to converted
@@ -50,16 +46,14 @@ def prepTrigramsFromFile(file_name):
 #
 # The stories do not have punctuation.
 def begin():
-	# output = open('output_simple.txt', 'w')
-	output = open('output_complex.txt', 'w')
-	# prepTrigramsFromFile('alice-27.txt')
+	output = open('output.txt', 'w')
+	prepTrigramsFromFile('alice-27.txt')
 	prepTrigramsFromFile('doyle-27.txt')
 	prepTrigramsFromFile('doyle-case-27.txt')
-	# prepTrigramsFromFile('london-call-27.txt')
-	# prepTrigramsFromFile('melville-billy-27.txt')
-	# prepTrigramsFromFile('twain-adventures-27.txt')
+	prepTrigramsFromFile('london-call-27.txt')
+	prepTrigramsFromFile('melville-billy-27.txt')
+	prepTrigramsFromFile('twain-adventures-27.txt')
 	firstWord = random.choice(GLOB_DICT.keys())
-	# firstWord = 'The'
 	output.write(''.join([firstWord,' ']))
 	for i in range(500):
 		secondWord = random.choice(GLOB_DICT[firstWord].keys())
